7d7a03296f0acb9cc53b4f8bc227c08936c067c9
Chernykh Ivan <Ivan.Chernykh@kaspersky.com>
2023-05-21 16:13:52 +0300

    add words controller

---
+++ Sources/App/Controllers/WordsController.swift
@@ -0,0 +1,25 @@
+import Fluent
+import Vapor
+
+struct WordsController: RouteCollection {
+
+    func boot(routes: RoutesBuilder) throws {
+        let wordsRoutes = routes.grouped("words")
+        wordsRoutes.group(":number") { room in
+            room.get(use: getWords)
+        }
+    }
+
+    func getWords(req: Request) async throws -> [String] {
+        let _ = try await TokenHelpers.getUserID(req: req)
+
+        guard let strNum = req.parameters.get("number"), let num = Int(strNum) else {
+            throw Abort(.badRequest)
+        }
+
+        let words = WordsProvider.getRandomWords(num: num)
+        return words
+    }
+
+}
+
